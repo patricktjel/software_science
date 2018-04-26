5fb1f32c81b48cd6743e97958fc552eec5e6f58f
Patrick Thijssen <patrickthijssen27@gmail.com>
2018-04-26 11:45:19 +0200

    software science project create week 1

---
+++ mutex/cmake-build-debug/CMakeFiles/mutex.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mutex.dir/main.c.o"
+  "mutex.pdb"
+  "mutex"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mutex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
