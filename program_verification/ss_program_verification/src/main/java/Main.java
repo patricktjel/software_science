32383102d02aa7fdd0fc33e2b152c0342aeadcf1
Patrick Thijssen <patrickthijssen27@gmail.com>
2018-06-07 15:28:26 +0200

    merge

--- program_verification/ss_program_verification/src/main/java/Main.java
+++ program_verification/ss_program_verification/src/main/java/Main.java
@@ -1,14 +1,15 @@
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.stmt.BlockStmt;
 
-import java.util.Optional;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
-        CompilationUnit compilationUnit = JavaParser.parse("class A { }");
-        Optional<ClassOrInterfaceDeclaration> classA = compilationUnit.getClassByName("A");
+//https://tomassetti.me/parsing-in-java/#javaLibraries
+    public static void main(String[] args) throws IOException {
+        CompilationUnit compilationUnit = JavaParser.parseResource("Example.java");
+        BlockStmt method = (BlockStmt) compilationUnit.getChildNodes().get(0).getChildNodes().get(1).getChildNodes().get(3);
         System.out.println("hello");
     }
 }
