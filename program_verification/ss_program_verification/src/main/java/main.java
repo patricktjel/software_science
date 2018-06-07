1630fd0d5821fbec3787e126946fbf903c4336c6
Patrick Thijssen <patrickthijssen27@gmail.com>
2018-06-07 15:04:56 +0200

    .

--- program_verification/ss_program_verification/src/main/java/main.java
+++ program_verification/ss_program_verification/src/main/java/main.java
@@ -1,6 +1,14 @@
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+
+import java.util.Optional;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Boo!");
+        CompilationUnit compilationUnit = JavaParser.parse("class A { }");
+        Optional<ClassOrInterfaceDeclaration> classA = compilationUnit.getClassByName("A");
+        System.out.println("hello");
     }
 }
