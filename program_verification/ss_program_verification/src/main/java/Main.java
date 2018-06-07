6e2151df02e072a54acf488f35d658e25121e3a9
Patrick Thijssen <patrickthijssen27@gmail.com>
2018-06-07 16:11:13 +0200

    z3 bindings are binded.

--- program_verification/ss_program_verification/src/main/java/Main.java
+++ program_verification/ss_program_verification/src/main/java/Main.java
@@ -1,6 +1,9 @@
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.stmt.BlockStmt;
+import com.microsoft.z3.*;
 
 import java.io.IOException;
 
@@ -9,7 +12,17 @@ public class Main {
 //https://tomassetti.me/parsing-in-java/#javaLibraries
     public static void main(String[] args) throws IOException {
         CompilationUnit compilationUnit = JavaParser.parseResource("Example.java");
-        BlockStmt method = (BlockStmt) compilationUnit.getChildNodes().get(0).getChildNodes().get(1).getChildNodes().get(3);
-        System.out.println("hello");
+        MethodDeclaration method = (MethodDeclaration) compilationUnit.getChildNodes().get(0).getChildNodes().get(1);
+        BlockStmt code = (BlockStmt) method.getChildNodes().get(method.getChildNodes().size() - 1);
+        parseMethodToSSA(code);
+    }
+
+    static void parseMethodToSSA(Node node) {
+        parseToZ3();
+    }
+
+    static void parseToZ3 () {
+        Context context = new Context();
+        context.close();
     }
 }
