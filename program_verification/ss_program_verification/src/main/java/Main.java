c6d97a229f6dc2fdb2bfb67be74f45a126bffabf
Lars Meijer <lars_meijer@outlook.com>
2018-06-12 12:09:18 +0200

    pom edit

--- program_verification/ss_program_verification/src/main/java/Main.java
+++ program_verification/ss_program_verification/src/main/java/Main.java
@@ -14,7 +14,6 @@ import com.microsoft.z3.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Main {
