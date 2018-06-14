6f715ea6b3c2d7d1332783af3cb1a63d8088550f
Lars Meijer <lars_meijer@outlook.com>
2018-06-14 13:48:43 +0200

    Week2 deliverable

---
+++ program_verification/week_2/patrick_lars_program_verification/ss_program_verification_source/src/main/java/Tree.java
@@ -0,0 +1,62 @@
+public class Tree<T> {
+    private T data;
+    private Tree<T> left;
+    private Tree<T> right;
+
+    public Tree(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public Tree<T> getLeft() {
+        return left;
+    }
+
+    public Tree<T> getRight() {
+        return right;
+    }
+
+    public void addLeftNode(Tree<T> toAdd) {
+        if (left != null) {
+            throw new RuntimeException("Tree already has a right node");
+        }
+        this.left = toAdd;
+    }
+
+    public void addRightNode(Tree<T> toAdd) {
+        if (right != null) {
+            throw new RuntimeException("Tree already has a right node");
+        }
+        this.right = toAdd;
+    }
+
+    public void replace(T oldValue, T newValue) {
+        if (this.data.equals(oldValue)) {
+            this.data = newValue;
+        }
+        if (left != null) {
+            this.left.replace(oldValue, newValue);
+        }
+        if (right != null) {
+            this.right.replace(oldValue, newValue);
+        }
+    }
+
+    public void print(int depth) {
+
+        for (int i = 0; i < depth; i++) {
+            System.out.print("\t");
+        }
+        System.out.println(data);
+
+        if (left != null) {
+            left.print(depth + 1);
+        }
+        if (right != null) {
+            right.print(depth + 1);
+        }
+    }
+}
