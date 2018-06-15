11410bbefd5480776eb91c78a6b07918cc9a453f
Patrick Thijssen <patrickthijssen27@gmail.com>
2018-06-15 11:44:38 +0200

    updated example 3.

--- program_verification/ss_program_verification/src/main/resources/Example_3.java
+++ program_verification/ss_program_verification/src/main/resources/Example_3.java
@@ -3,7 +3,7 @@ public class Example {
     public int count_to_five(int n1, int n2) {
         int i = 0;
         while (i < 5) {
-//            i >= 0 && i <= 3
+//            i >= 0 && i <= 3; i
             int i = i + 1;
         }
         return i;
